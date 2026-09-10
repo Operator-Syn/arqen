baa658646ccc71c9b84a457fb866232d409733cc
Operator-Syn <johnronanbeira100404@gmail.com>
2026-09-10 16:16:29 +0800

    Define Arqen development shell

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.arqen = pkgs.mkShell {
+          name = "arqen";
+          packages = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
+          ];
+
+          RUST_BACKTRACE = "1";
+
+          shellHook = ''
+            export name="arqen"
+          '';
+        };
+      });
+}
