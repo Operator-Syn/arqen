816d951c610efcc6fbbeab577703efbd098bc635
Operator-Syn <johnronanbeira100404@gmail.com>
2026-09-22 16:00:24 +0800

    Add src/tui/app/disconnect.rs.

---
+++ src/tui/app/disconnect.rs
@@ -0,0 +1,18 @@
+impl App {
+    fn handle_disconnect_key(&mut self, key: KeyEvent) {
+        let Screen::ConfirmDisconnect { subject, .. } = &self.screen else {
+            return;
+        };
+        match key.code {
+            KeyCode::Enter | KeyCode::Char('y') => {
+                let subject = subject.clone();
+                self.disconnect_account(&subject);
+            }
+            KeyCode::Esc | KeyCode::Char('n') => {
+                self.screen = Screen::Accounts;
+                self.notice = Some("Disconnect cancelled.".into());
+            }
+            _ => {}
+        }
+    }
+}
