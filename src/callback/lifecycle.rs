e95334caf60090a679f5d592e8e79bc17143b75d
Operator-Syn <johnronanbeira100404@gmail.com>
2026-09-22 16:00:22 +0800

    Add src/callback/lifecycle.rs.

---
+++ src/callback/lifecycle.rs
@@ -0,0 +1,8 @@
+impl Drop for CallbackServer {
+    fn drop(&mut self) {
+        self.shutdown.store(true, Ordering::Relaxed);
+        if let Some(thread) = self.thread.take() {
+            let _ = thread.join();
+        }
+    }
+}
