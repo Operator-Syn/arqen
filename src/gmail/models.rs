519dca322278904523e8a8a9bc3a82cee2d3b894
Operator-Syn <johnronanbeira100404@gmail.com>
2026-09-23 19:52:50 +0800

    Add Gmail message read models.

--- src/gmail/models.rs
+++ src/gmail/models.rs
@@ -17,3 +17,31 @@ pub struct EmailListResponse {
     pub next_page_token: Option<String>,
     pub result_size_estimate: Option<u32>,
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize, JsonSchema, PartialEq, Eq)]
+#[serde(rename_all = "snake_case")]
+pub enum EmailBodyStatus {
+    Complete,
+    NoReadableBody,
+    Incomplete,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, JsonSchema, PartialEq, Eq)]
+pub struct EmailRecipients {
+    pub to: Vec<String>,
+    pub cc: Vec<String>,
+    pub bcc: Vec<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, JsonSchema, PartialEq, Eq)]
+pub struct EmailReadResponse {
+    pub message_id: String,
+    pub thread_id: String,
+    pub from: Option<String>,
+    pub recipients: EmailRecipients,
+    pub date: Option<String>,
+    pub subject: Option<String>,
+    pub labels: Vec<String>,
+    pub body_text: Option<String>,
+    pub body_status: EmailBodyStatus,
+}
